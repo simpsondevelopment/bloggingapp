3dcff920b3202ca1b4228f379754de2253d1d0a1
simpson1177 <34554378+simpson1177@users.noreply.github.com>
2021-03-28 11:37:48 -0600

    extract prisma secret to env variable

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import { fragmentReplacements } from "./resolvers/index";
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: "Liverp@@l11",
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements,
 });
 
